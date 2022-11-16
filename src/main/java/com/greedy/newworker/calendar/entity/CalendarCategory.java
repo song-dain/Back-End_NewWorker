d6585b82a9397184bb31f9b96c6221adf18e7188
송다인 <inhere31@gmail.com>
2022-11-16 16:39:28 +0900

    송다인 캘린더 카테고리 엔티티 추가

--- src/main/java/com/greedy/newworker/calendar/entity/CalendarCategory.java
+++ src/main/java/com/greedy/newworker/calendar/entity/CalendarCategory.java
@@ -1,5 +1,27 @@
 package com.greedy.newworker.calendar.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "TBL_CALENDAR_CATEGORY")
 public class CalendarCategory {
+	
+	@Id
+	@Column(name = "CALENDAR_CATEGORY_NO")
+	private Long calendarCategoryNo;
+	
+	@Column(name = "CATEGORY_NAME")
+	private String categoryName;
+	
 
 }
