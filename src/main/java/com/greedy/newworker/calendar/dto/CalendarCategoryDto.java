481ff30745c024da45c8cee87934e6c3439f53d3
송다인 <inhere31@gmail.com>
2022-11-16 16:35:09 +0900

    송다인 Message Calendar Entity Dto 생성

---
+++ src/main/java/com/greedy/newworker/calendar/dto/CalendarCategoryDto.java
@@ -0,0 +1,11 @@
+package com.greedy.newworker.calendar.dto;
+
+import lombok.Data;
+
+@Data
+public class CalendarCategoryDto {
+	
+	private String calendarCategoryNo;
+	private String categoryNo;
+
+}
