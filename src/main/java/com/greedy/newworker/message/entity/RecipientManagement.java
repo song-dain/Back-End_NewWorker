d5cbad0d0328e6c77577e00d38a689c8efea09f4
송다인 <inhere31@gmail.com>
2022-11-17 19:18:43 +0900

    송다인 import 수정

--- src/main/java/com/greedy/newworker/message/entity/RecipientManagement.java
+++ src/main/java/com/greedy/newworker/message/entity/RecipientManagement.java
@@ -2,13 +2,9 @@ package com.greedy.newworker.message.entity;
 
 import java.io.Serializable;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.MapsId;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.DynamicInsert;
