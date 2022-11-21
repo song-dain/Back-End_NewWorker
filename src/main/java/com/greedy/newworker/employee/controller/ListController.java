32a2854875125e88b373451f0178ee55107454ca
박상연 <sy23833@gmail.com>
2022-11-21 16:48:14 +0900

    직원조회코드추가

--- src/main/java/com/greedy/newworker/employee/controller/ListController.java
+++ src/main/java/com/greedy/newworker/employee/controller/ListController.java
@@ -1,18 +1,69 @@
-//package com.greedy.newworker.employee.controller;
-//
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import lombok.extern.slf4j.Slf4j;
-//
-//@Slf4j
-//@RestController
-//@RequestMapping("/api/v1")
-//public class ListController {
-//
-//	private final ListService listService;
-//	
-//	public ListController(ListService listService) {
-//		this.listService = listService;
+package com.greedy.newworker.employee.controller;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.greedy.newworker.common.ResponseDto;
+import com.greedy.newworker.common.paging.Pagenation;
+import com.greedy.newworker.common.paging.PagingButtonInfo;
+import com.greedy.newworker.common.paging.ResponseDtoWithPaging;
+import com.greedy.newworker.employee.dto.EmployeeDto;
+import com.greedy.newworker.employee.service.ListService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/emp")
+public class ListController {
+
+	private final ListService listService;
+	
+	public ListController(ListService listService) {
+		this.listService = listService;
+	}
+	
+	/*직원 조회*/
+	@GetMapping("/employeeList")
+	public ResponseEntity<ResponseDto> selectEmployeeList(@RequestParam(name="page", defaultValue="1") int page) {
+		
+		log.info("[EmployeeController] selectEmployeeList Start ================================");
+		log.info("[EmployeeController] page : {}", page);
+		
+		Page<EmployeeDto> employeeDtoList = listService.selectEmployeeList(page);
+		
+		PagingButtonInfo pageInfo = Pagenation.getPagingButtonInfo(employeeDtoList);
+		 
+		log.info("[EmployeeController] pageInfo : {}", pageInfo);
+		
+		ResponseDtoWithPaging responseDtoWithPaging = new ResponseDtoWithPaging();
+		responseDtoWithPaging.setPageInfo(pageInfo);
+		responseDtoWithPaging.setData(employeeDtoList.getContent());
+		
+		log.info("[EmployeeController] selectEmployeeList End ================================");
+		
+		return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "조회 성공", responseDtoWithPaging));
+	}
+	
+//	/* 직원상세조회(관리자) */
+//	@GetMapping("/employeeList/detail-management/{employeeNo}")
+//	public ResponseEntity<ResponseDto> selectEmployeeListDetail(@PathVariable Long employeeNo) {
+//		
+//		return ResponseEntity
+//				.ok()
+//				.body(new ResponseDto(HttpStatus.OK, "직원 상세 조회 성공", listService.selectEmployeeForAdmin(employeeNo)));
 //	}
-//}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/greedy/newworker/employee/repository/ListRepository.java
@@ -0,0 +1,25 @@
+package com.greedy.newworker.employee.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.greedy.newworker.employee.entity.Employee;
+
+public interface ListRepository extends JpaRepository<Employee, Long> {
+
+	/* 회원조회(전체정보) */
+	@EntityGraph(attributePaths= {"employeeNo"})
+	Page<Employee> findAll(Pageable pageable);
+	
+//	/* 상세조회(관리자)*/
+//	@Query("SELECT e " +
+//			 "FROM Employee e" +
+//			"WHERE e.employeeNo = :employeeNo ")
+//	Optional<Employee> findByEmployeeNo(@Param("employeeNo") Long employeeNo);
+}
