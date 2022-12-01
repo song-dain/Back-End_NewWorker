2353624c3208d79258f795aa1fb497912f195bcf
박상연 <sy23833@gmail.com>
2022-12-01 17:48:19 +0900

    휴가 등록 수정 조회 완료

--- src/main/java/com/greedy/newworker/rest/controller/RestsController.java
+++ src/main/java/com/greedy/newworker/rest/controller/RestsController.java
@@ -3,6 +3,7 @@ package com.greedy.newworker.rest.controller;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.greedy.newworker.common.ResponseDto;
 import com.greedy.newworker.common.paging.Pagenation;
 import com.greedy.newworker.common.paging.PagingButtonInfo;
 import com.greedy.newworker.common.paging.ResponseDtoWithPaging;
+import com.greedy.newworker.employee.dto.EmployeeDto;
 import com.greedy.newworker.rest.dto.RestDto;
 import com.greedy.newworker.rest.service.RestService;
 
@@ -31,6 +33,7 @@ public class RestsController {
 	public RestsController(RestService restService) {
 		this.restService = restService;
 	}
+	
 	
 	/* 조회 */
 	@GetMapping("/list")
@@ -50,14 +53,19 @@ public class RestsController {
 
 	/* 등록 */
 	@PostMapping("/regist")
-	public ResponseEntity<ResponseDto> insertRest(@ModelAttribute RestDto restDto) {
+	public ResponseEntity<ResponseDto> insertRest(@ModelAttribute RestDto restDto,
+			@AuthenticationPrincipal EmployeeDto employee) {
 		
-		return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "휴가 등록 성공", restService.insertRest(restDto)));
+		restDto.setEmployeeNo(employee);
+		
+		
+		return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "휴가 등록 성공", restService.insertRest(restDto, employee)));
 	}
 	
 	/* 수정 */
 	@PutMapping("/regist")
-	public ResponseEntity<ResponseDto> updateRest(@ModelAttribute RestDto restDto) {
+	public ResponseEntity<ResponseDto> updateRest(@ModelAttribute RestDto restDto,
+			@AuthenticationPrincipal EmployeeDto employee) {
 		
 		return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "휴가 수정 성공", restService.updateRest(restDto)));
 	}
@@ -97,9 +105,10 @@ public class RestsController {
 	
 	/* 연차 승인, 반려 */	
 	@PutMapping("/list/admin/ok")
-	public ResponseEntity<ResponseDto> updateRestOk(@ModelAttribute RestDto restDto) {
+	public ResponseEntity<ResponseDto> updateRestOk(@ModelAttribute RestDto restDto) {		
 		
-		return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "휴가 수정 성공", restService.updateRestOk(restDto)));
+		
+		return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "완료되었습니다.", restService.updateRestOk(restDto)));
 	}
 	
 }
