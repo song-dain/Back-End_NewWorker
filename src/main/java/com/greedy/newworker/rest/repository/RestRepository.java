31db51650bb1d7be15a3c8e5d73148f3eee955fb
송다인 <inhere31@gmail.com>
2022-12-04 22:02:48 +0900

    송다인 오늘일정조회 기능 추가

--- src/main/java/com/greedy/newworker/rest/repository/RestRepository.java
+++ src/main/java/com/greedy/newworker/rest/repository/RestRepository.java
@@ -8,9 +8,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.greedy.newworker.employee.entity.Employee;
 import com.greedy.newworker.rest.entity.Rest;
+
+import io.lettuce.core.dynamic.annotation.Param;
 
 public interface RestRepository extends JpaRepository<Rest, Long>{
 
@@ -25,5 +28,12 @@ public interface RestRepository extends JpaRepository<Rest, Long>{
 	
 	/* [calendar] 사원별 승인 연차 정보 */
 	List<Rest> findByEmployeeNoAndRestOk(Employee employeeNo, String restOk);
+
+	/* [메인] 사원별 오늘 연차 정보 */
+	@Query(value="SELECT * FROM TBL_REST "
+			+ "WHERE EMPLOYEE_NO =:employeeNo AND REST_OK = 'Y' "
+			+ "AND TO_CHAR(SYSDATE, 'yy/MM/dd') >= TO_CHAR(REST_FDATE, 'yy/MM/dd') "
+			+ "AND TO_CHAR(SYSDATE, 'yy/MM/dd') <= TO_CHAR(REST_LDATE, 'yy/MM/dd')", nativeQuery=true)
+	List<Rest> findByEmployeeNoAndRestOkAndRestFdate(@Param("employeeNo")Long employeeNo);
 	
 }
